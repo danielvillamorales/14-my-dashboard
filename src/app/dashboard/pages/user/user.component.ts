5f3f2d5fba04230cffd7baa609d22638c5efbe43
danielvillamorales <daniel.villa@utp.edu.co>
2024-11-25 11:34:16 -0500

    title label

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TitleComponent } from '@shared/title/title.component';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -9,7 +9,7 @@ import { Service } from '../../../services/.service';
 @Component({
   standalone: true,
   imports: [CommonModule, TitleComponent],
-  template: `<app-title title="User"></app-title>
+  template: `<app-title [title]="titleLabel()"></app-title>
     @if (user()) {
     <section>
       <img [srcset]="user()!.avatar" alt="" />
@@ -29,4 +29,11 @@ export default class UserComponent {
       switchMap(({ id }) => this.usersService.getUserById(id))
     )
   );
+
+  public titleLabel = computed(() => {
+    if (this.user()) {
+      return `User: ${this.user()?.first_name} ${this.user()?.last_name}`;
+    }
+    return 'User';
+  });
 }
